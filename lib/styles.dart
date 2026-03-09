e42f7c1adbaa3b3bda3d1d7032c5d458c2b25be2
annemievdc <annemie.vdc@gmail.com>
2026-03-09 18:34:59 +0100

    oefening2

---
+++ lib/styles.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  TextStyle verdanaNormal12 = TextStyle(
+    fontFamily: 'Verdana',
+    fontSize: 12,
+    fontWeight: FontWeight.normal,
+  );
+  TextStyle verdanaBold24 = TextStyle(
+    fontFamily: 'Verdana',
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+
+}
